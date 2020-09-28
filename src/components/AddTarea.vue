99dee4ac56a620214d6bb78453789582c50b4c0c
Diana Munteanu <diana.munteanu@nodrizatech.com>
2020-09-28 11:49:29 +0200

    Add functionality

---
+++ src/components/AddTarea.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+      <input type="text" v-model="title" name="title">
+      <button @click="addTarea">Añadir</button>
+  </div>
+</template>
+
+<script>
+import { v4 as uuidv4 } from 'uuid';
+
+export default {
+  name: "AddTarea.vue",
+  data() {
+    return {
+      title: ''
+    }
+  },
+  methods: {
+    addTarea(){
+
+      const nuevaTarea = {
+        id: uuidv4(),
+        title: this.title,
+        completed: false
+      }
+
+      this.$emit('add-tarea', nuevaTarea);
+      this.title = '';
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
