c050a6e983d6609d821ced97390ec34e06816a1a
Diana Munteanu <diana.munteanu@nodrizatech.com>
2020-09-28 11:54:01 +0200

    Add components tarea and tareas

---
+++ src/components/Tarea.vue
@@ -0,0 +1,21 @@
+<template>
+  <div v-bind:class="{ 'completed': tarea.completed }">
+      <p @click="markComplete">{{ tarea.title }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Tarea',
+  props: [
+      "tarea"
+  ],
+  methods: {
+    markC
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Tareas.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h2>Mi lista de tareas</h2>
+    <ul>
+      <li v-bind:key="tarea.id" v-for="tarea in tareas">
+        <Tarea v-bind:tarea="tarea" />
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Tarea from './Tarea';
+
+export default {
+  name: 'Tareas',
+  components: {
+    Tarea
+  },
+  props: [
+      "tareas"
+  ]
+}
+</script>
+
+<style scoped>
+</style>
