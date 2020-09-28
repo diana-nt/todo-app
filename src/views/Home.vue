6e2e9f4417ab9f3505058f978c55859d96784bbd
Diana Munteanu <diana.munteanu@nodrizatech.com>
2020-09-28 11:53:18 +0200

    Add unversioned files init app

---
+++ src/views/Home.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="home">
+    <tareas v-bind:tareas="tareas"></tareas>
+    <addTarea v-on:add-tarea="addTarea" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Tareas from "@/components/Tareas";
+import AddTarea from "@/components/AddTarea";
+
+export default {
+  name: 'Home',
+  components: {
+    Tareas,
+    AddTarea
+  },
+  data() {
+    return {
+      tareas: [
+        {
+          id: 1,
+          title: 'Aprender Vue',
+          completed: false
+        },
+        {
+          id: 2,
+          title: 'Lavar coche',
+          completed: false
+        },
+        {
+          id: 3,
+          title: 'Pasar la aspiradora',
+          completed: false
+        },
+        {
+          id: 4,
+          title: 'Hacer trabajo de clase',
+          completed: false
+        },
+        {
+          id: 5,
+          title: 'Comprar impresora',
+          completed: false
+        }
+      ],
+    }
+  },
+  methods: {
+    addTarea(nuevaTarea) {
+      console.log(nuevaTarea)
+      this.tareas = [...this.tareas, nuevaTarea];
+    }
+  }
+}
+</script>
